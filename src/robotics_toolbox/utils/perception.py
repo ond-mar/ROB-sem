1bbca96e52c305224bea3c00c100b316867660fa
Ondrej Marek <ondrej.marek10@gmail.com>
2025-11-01 08:37:10 +0100

    hw04 - Perception

--- src/robotics_toolbox/utils/perception.py
+++ src/robotics_toolbox/utils/perception.py
@@ -26,11 +26,36 @@ def find_hoop_homography(images: ArrayLike, hoop_positions: List[dict]) -> np.nd
     ...
     ]
     """
-
+    # Check inputs
     images = np.asarray(images)
     assert images.shape[0] == len(hoop_positions)
 
-    # todo HW03: Detect circle in each image
-    # todo HW03: Find homography using cv2.findHomography. Use the hoop positions and circle centers.
+    img_centers = np.empty((len(hoop_positions), 2))
 
-    return np.eye(3)
+    # todo HW03: Detect circle in each image
+    for i, img in enumerate(images):
+        # highlight blue hoop
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # convert to HSV
+        imgHSV = cv2.medianBlur(imgHSV, 5) # apply median blur to reduce noise
+        
+        # desired color: HSV = 209, 57, 45 (in standard range 0-360, 0-100, 0-100)
+        # in OpenCV range: HSV = 104, 145, 114
+        lower = np.array([90, 50, 90])
+        upper = np.array([120, 200, 160])
+        mask = cv2.inRange(imgHSV, lower, upper)
+      
+        circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp=1, minDist=300, param1=100, param2=30, minRadius=10, maxRadius=400)               
+        assert circles is not None, "No circles found"     
+
+        circles = np.uint16(np.around(circles)) 
+
+        circle = circles[0][0] 
+        img_centers[i] = [circle[0], circle[1]]  # (x, y)
+
+    ref_centers = np.empty((len(hoop_positions), 2))
+    for i, hoop in enumerate(hoop_positions):
+        pos = hoop["translation_vector"]
+        ref_centers[i] = [pos[0], pos[1]] 
+
+    homography = cv2.findHomography(img_centers, ref_centers, cv2.RANSAC)[0]
+    return homography
