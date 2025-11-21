13cd2303cf988e858ded3019999255f6837404f3
Bublinka <deez@nuts.com>
2025-11-21 18:23:18 +0100

    Added homography

--- semestral/camera.py
+++ semestral/camera.py
@@ -9,7 +9,6 @@ class CameraHelper:
         self.prefix = prefix
         self.robot = robot
 
-    def capture_image(self, name: str | None, save: bool = True):
-        timestamp = datetime.now().isoformat()
+    def capture_image(self, name: str, save: bool = True):
         image = self.robot.grab_image()
-        cv2.imwrite(self.prefix + "_" + timestamp + ".png", image)
+        cv2.imwrite(name + ".png", image)
