e8fe63824696420d60eb8efd0a88aad1c7da5f11
Bublinka <deez@nuts.com>
2025-11-30 17:16:18 +0100

    draft calibration sequence

--- semestral/hoop_images2.py
+++ semestral/hoop_images2.py
@@ -1,39 +1,88 @@
 from ctu_bosch_sr450 import RobotBosch
+from camera import CameraHelper
+from utils import LEFT_Q, RIGHT_Q, MIN_Z_Q, MAX_Z_Q
+import math
+import numpy as np
+import cv2
+
+DEFAULT_INCREMENT = 0.1
+DEBUG = True
+
+def save_matrix(name, matrix):
+    np.save("./calibration_data/" + name, matrix)
+
+def rotate(r: RobotBosch, increment = 0.1):
+    q = r.get_q()
+    q[3] = q[3] + increment
+    r.move_to_q(q)
+    r.wait_for_motion_stop()
+
+def shift_height(r: RobotBosch, new_h_q: float):
+    q = r.get_q()
+    q[2] = new_h_q
+    r.move_to_q(q)
+    r.wait_for_motion_stop()
+
+def find_center(img: list) -> list | None:
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.medianBlur(img, 5)
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp=1, minDist=300, param1=100, param2=30, minRadius=50, maxRadius=400)
+
+    if circles is None:
+        print("Could not find circles")
+        return None
+
+    circles = np.uint16(np.around(circles))
+
+    return circles[0][0]
+
+def get_data_in_pose(r: RobotBosch, camera: CameraHelper):
+    for i in range(math.floor((np.pi * 2) / DEFAULT_INCREMENT)):
+        rotate(r, DEFAULT_INCREMENT)
+        img = camera.get_image()
+
+        if DEBUG:
+            im_small = cv2.resize(img, (960, 540))
+            cv2.imshow("output", im_small)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows() 
+
+        center = find_center(img)
+
+        if center is None:
+            return
+
+        pose = list(r.fk(r.get_q()))
+
+        save_matrix("robot_pose_" + str(i), pose)
+        save_matrix("circle_coords_" + str(i), center)
+
 
 robot = RobotBosch()
 robot.initialize()
+camera = CameraHelper(robot, "")
 
-Z_INCREMENT = -0.03
-Y_INCREMENT = -0.1
-X_INCREMENT = -0.1
+robot.move_to_q(LEFT_Q)
+robot.wait_for_motion_stop()
 
-start_pose = robot.fk(robot.get_q())
+get_data_in_pose(robot, camera)
 
-def find_closest_q(c, all_q):
-    current_dif = float("inf")
-    current_q = None
+shift_height(robot, MIN_Z_Q)
 
-    for q in all_q:
-        abs_dif = abs(q[0] - c[0]) + abs(q[1] - c[1])
-        if abs_dif < current_dif:
-            current_q = q
+get_data_in_pose(robot, camera)
 
-    return current_q
-    
+shift_height(robot, MAX_Z_Q)
 
-for i in range(10):
-    newq = robot.ik_xyz(start_pose[0] + i * X_INCREMENT, start_pose[1] + i * Y_INCREMENT, start_pose[2])
-    
-    if len(newq) > 0:
-        selected = find_closest_q(robot.get_q(), newq)
-        robot.move_to_q(selected)
-        robot.wait_for_motion_stop()
+r.move_to_q(RIGHT_Q)
+r.wait_for_motion_stop()
 
-    pose = robot.fk(robot.get_q())
+get_data_in_pose(robot, camera)
 
-    for z in range(10):
-        new_z_q = robot.ik_xyz(pose[0], pose[1], pose[2] + z * Z_INCREMENT)
-        if len(new_z_q) > 0:
-            selected = find_closest_q(robot.get_q(), new_z_q)
-            robot.move_to_q(selected)
-            robot.wait_for_motion_stop()
+shift_height(robot, MIN_Z_Q)
+
+get_data_in_pose(robot, camera)
+
+shift_height(robot, MAX_Z_Q)
+
+robot.hard_home()
+robot.close()
--- semestral/utils.py
+++ semestral/utils.py
@@ -1,8 +1,14 @@
 import numpy as np
 import cv2
 
-MIN_Z = 0.200
-MIN_Z_Q = - 0.30
+MAX_Z = 0.5
+MAX_Z_Q = 0
+
+MIN_Z = 0.8
+MIN_Z_Q = - 0.33
+
+LEFT_Q = [-4.83117755e-01, -6.83617751e-01,  2.25225225e-05, -7.05254062e-04]
+RIGHT_Q = [1.08819848e+00, -6.83617751e-01,  2.47747748e-05, -6.17097304e-04]
 
 def load_matrix(name):
     with open(name, "rb") as f:
