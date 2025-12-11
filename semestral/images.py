b09c7f65c22d1757df4714b4301c3583b346522a
Ondrej Marek <ondrej.marek10@gmail.com>
2025-12-11 10:47:34 +0100

    Modified image name

--- semestral/images.py
+++ semestral/images.py
@@ -4,9 +4,9 @@ from camera import CameraHelper
 robot = RobotBosch()
 robot.initialize(home=False)
 
-camera_helper = CameraHelper(robot, "board")
+camera_helper = CameraHelper(robot, "img")
 
-camera_helper.capture_image_date("board")
+camera_helper.capture_image_date("img")
 
 robot.release()
 robot.close()
